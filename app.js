fb8177bc25d817c8253d4b7b25ba0af6dd41c4e3
Siddharth Suri <siddharth2005suri@gmail.com>
2026-06-17 16:08:29 +0530

    minor changes

--- app.js
+++ app.js
@@ -1,8 +1,5 @@
-class getValues {
-    device 
-    localSDP
-    offer 
-
+const connection = new connectToWebRTC('stun:stun.l.google.com:19302');
+class connectToWebRTC {
 
     constructor(stunServerUrl){
         this.device = new RTCPeerConnection({
@@ -13,14 +10,33 @@ class getValues {
         this.localSDP = null
     }
 
+    establishConnectionRemote(){
+        const value =  document.getElementById("remote-string").value
+        this.device.setRemoteDescription(value)
+
+    }
     async establishConnectionLocal(){
         this.offer = await this.device.createOffer()
         await this.device.setLocalDescription(this.offer)
 
         await new Promise((resolve)=>{
             this.device.onicecandidate = (event) => {
-                
+                // this needs to fire when all candidates are completed 
+                // because then the final ice candidate is null which
+                // ticks off the if condition
+                if(!event.candidate){
+                    resolve()
+                }
             }
         })
+
+        this.localSDP = this.device.localDescription.sdp
+        console.log("SDP String: "+ this.localSDP)
+        return 
     }
+}
+
+function createOffer() {
+    connection.establishConnectionLocal();
+    document.getElementById('offer-out').value = sdp;
 }
